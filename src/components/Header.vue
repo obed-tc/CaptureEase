f12b0074001d172e4f365076182db694af8f19d7
obed-tc <castroobed53@gmail.com>
2024-04-07 13:55:11 -0400

    feat: Add Header and Logo

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <nav class="navbar navbar-dark bg-dark px-3">
+        <a class="navbar-brand" href="#">
+            <img src="/Logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            Capture Ease
+        </a>
+    </nav>
+</template>
