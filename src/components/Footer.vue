30ef55def05be6589aef401579238e7e385bd424
obed-tc <castroobed53@gmail.com>
2024-04-07 13:54:33 -0400

    feat: Add Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,47 @@
+<template>
+    <!-- Footer -->
+    <footer class="bg-body-tertiary text-center">
+        <!-- Grid container -->
+        <div class="container p-4">
+            <!-- Section: Social media -->
+            <section class="">
+                <!-- Facebook -->
+                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"><i
+                        class="bi bi-facebook"></i></a>
+
+                <!-- Twitter -->
+                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"><i
+                        class="bi bi-twitter-x"></i></a>
+
+                <!-- Instagram -->
+                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"><i
+                        class="bi bi-instagram"></i></a>
+
+                <!-- Linkedin -->
+                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"><i
+                        class="bi bi-linkedin"></i></a>
+
+                <!-- Github -->
+                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"><i
+                        class="bi bi-github"></i></a>
+            </section>
+
+            <!-- Section: Text -->
+            <section class="">
+                <p>
+                    ¡Captura y Comparte Tu Mundo Digital! 🌐✨
+                </p>
+            </section>
+            <!-- Section: Text -->
+        </div>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+            © 2024 Copyright:
+            <a class="text-reset fw-bold" href="https://github.com/obed-tc">Capture Ease</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+</template>
